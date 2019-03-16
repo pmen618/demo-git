b1bc12b460b22081363bcc63f73fe761460b09c4
Pmen <pmen618@gmail.com>
2019-03-16 18:07:18 +0700

    Imporve cat method

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var Dog = require('./Dog');
-var dog = new Dog('Min');
+var Mouse = require('./Mouse');
+var Mouse = require('./Cat');
+
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog('');
+try{
+	cat.eat(dog);
+}
+catch(error){
+	console.log('Error error error');
+}
+cat.eat(mouse);
+console.log(mouse);
 dog.sayHi();
